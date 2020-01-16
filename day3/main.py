66e28bf3ef6fa5df19e543cdb3a05682197c4bee
VincentVelthuis <v.velthuis@gmail.com>
2020-01-16 11:17:50 +0100

    fixed main.py (from new init)

--- day3/main.py
+++ day3/main.py
@@ -1,42 +1,45 @@
-import numpy as np
+wire1 = "R8,U5,L5,D3".split(',')
+w1path = [[0,0]] #starts at origin
+wire2 = "U7,R6,D4,L4".split(',')
+w2path = [[0,0]] #starts at origin
 
-w1p="R8,U5,L5,D3".split(',')
-max1=[0,0]
-start=coord=max_coord=[0,0]
+def update_path(path, direction):
+  # take one step from the last coordinate of 'path'
+  # in any given 'direction'. 
+  #'new_path' is a concat of the step added to the original path.
+  coord = path[-1] #last coordinate as starting point
+  if direction == "L": 
+    new_path = path + [[coord[0]-1, coord[1]]]
+  elif direction == "R":
+    new_path = path + [[coord[0]+1,coord[1]]]
+  elif direction == "U":
+    new_path = path + [[coord[0],coord[1]+1]]
+  elif direction == "D":
+    new_path = path + [[coord[0],coord[1]-1]]
+  return new_path
 
+def closest_point(path):
+  coord = []
+  small_dist = 123456789
 
-def change_location(x,y,action):
-  if action[0]=='R':
-    return x+int(action[1]), y
-  elif action[0]=='U':
-    return x, y+int(action[1])
-  elif action[0]=='L':
-    return x-int(action[1]), y
-  elif action[0]=='D':
-    return x, y-int(action[1])
-  else:
-    return x,y
+  for x,y in path:
+    if x + y < small_dist:
+      small_dist = x + y
+      coord = [x,y]
+  return small_dist, coord
 
-def update_max_coord(x_0,y_0,x_new,y_new):
-  x_max = max(x_0,x_new)
-  y_max = max(y_0, y_new)
-  return x_max, y_max
+for direction,distance in wire1:
+  for step in range(0,int(distance)):
+    w1path = update_path(w1path,direction)
+  #print("End w1:",direction,distance)
 
+for direction,distance in wire2:
+  for step in range(0,int(distance)):
+    w2path = update_path(w2path,direction)
+  #print("End w2:",direction,distance)
 
+intersection = [value for value in w1path if value in w2path]
+intersection = intersection[1:] # remove origin from intersection
 
-for action in range(0,len(w1p)):
-  x,y = change_location(coord[0],coord[1],w1p[action])
-  x_max, y_max = update_max_coord(max_coord[0],max_coord[1],x,y)
-  print("(",coord[0],coord[1],") to (",x,y,"),[",x_max, y_max,"]")
-
-  coord = [x,y]
-  max_coord=[x_max,y_max]
-
-matrix1=np.zeros((max_coord[1]+1 , max_coord[0]+1))
-coord=[0,0]
-for action in range(0,len(w1p)):
-  x,y = change_location(coord[0],coord[1],w1p[action])
-  for i range(coord[0],x):
-    for j in range(coord[1],y):
-      print(i,j)
-print(matrix1)
+mh_dist,coord = closest_point(intersection)
+print("distance",mh_dist)
