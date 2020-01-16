85988899f49df87ad17b264506d235346ed25c1f
VincentVelthuis <v.velthuis@gmail.com>
2020-01-16 16:56:02 +0100

    Update ass4a.py

--- day4/ass4a.py
+++ day4/ass4a.py
@@ -3,31 +3,33 @@ from tqdm import tqdm
 low_lim = 206938
 up_lim = 679128
 
-def rule1(number):
+def rule1(num):
   # It is a six-digit number
-  return len(str(number)) == 6
+  # THIS IS ALWAYS TRUE FOR THIS INPUT
+  return len(num) == 6
 
 def rule2(number):
   # The value is within the range given in your puzzle input.
   ## THIS IS ALWAYS TRUE BY DEFINITION
   return True
 
-def rule3(number):
+def rule3(num):
   #Two adjacent digits are the same (like 22 in 122345)
-  string = str(number)
-  for i in range(len(string)-1):
-    return string[i] == string[i+1]
+  for i in range(len(num)-1):
+    if num[i] == num[i+1]:
+      return True
   return False
 
+def rule4(num):
+  # Going from left to right, the digits never decrease;
+  # they only ever increase or stay the same (like 111123 or 135679).
+  for i in range(len(num)-1):
+    if num[i] > num[i+1]:
+      return False
+  return True
 
-
-boolean_rule1 = 0
-boolean_rule3 = 0
-boolean_total = 0
+n_passwords = 0
 for number in tqdm(range(low_lim, up_lim)):
-  boolean_rule1 += rule1(number)
-  boolean_rule3 += rule3(number)
-  boolean_total += (rule1(number) and rule3(number))
-print(boolean_rule1)
-print(boolean_rule3)
-print(boolean_total)
+  num =str(number)
+  n_passwords += (rule4(num) and rule3(num) and rule2(num) and rule1(num))
+print(n_passwords,"possible passwords\n")
